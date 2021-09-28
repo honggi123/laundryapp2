44cd76d3c7090fa7f49cde9626f392efa0e7b176
ahrim <ds02270227@naver.com>
2021-09-28 23:08:36 +0900

    2

--- app/src/main/java/com/example/laundryapp/MainActivity.java
+++ app/src/main/java/com/example/laundryapp/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        EditText editText = findViewById(R.id.inputid);
+        EditText editText = findViewById(R.id.edit_id);
     }
 
 }
